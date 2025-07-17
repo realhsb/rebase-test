7c1701452661ca8febfb2e15701029607f675e2e
숩 <realhsb@gmail.com>
2025-07-17 14:53:28 +0900

    create project file

---
+++ rebase-test/rebase-test/rebase_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  rebase_testApp.swift
+//  rebase-test
+//
+//  Created by Soop on 7/17/25.
+//
+
+import SwiftUI
+
+@main
+struct rebase_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
